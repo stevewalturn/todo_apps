fa209d7c7c9fc1d83772ce581fcbad2185721f1c
stevewalturn <steve@walturn.com>
2024-12-25 15:33:22 +0800

    Update files (batch 1/1)

---
+++ src/lib/main/main_production.dart
@@ -0,0 +1,10 @@
+import 'package:todo_apps/main/bootstrap.dart';
+import 'package:todo_apps/models/enums/flavor.dart';
+import 'package:todo_apps/ui/views/app/app_view.dart';
+
+void main() {
+  bootstrap(
+    builder: () => const AppView(),
+    flavor: Flavor.production,
+  );
+}
